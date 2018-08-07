0eaf17ed8e610c68fd83593772b880d2e59e327c
Carlos Andrés Rengifo Escobar <carlosandrex@gmail.com>
2018-08-06 19:17:03 -0500

    basic sensors config

---
+++ src/api/request.js
@@ -0,0 +1,14 @@
+const API = `http://aqa.unloquer.org:8086/query/`;
+const database = `aqa`;
+
+const pollingRequest = function(signal, interval, timeout) {
+
+}
+
+const promise = new Promise((resolve, reject) {
+
+});
+
+const constructQuery = function() {
+
+};
