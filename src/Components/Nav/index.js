f48bdba316e94cb0eda5f6b9d8da85e2f6e10519
DESKTOP-1KKDKAO\filoc <filocol@gmail.com>
2018-08-09 22:16:09 -0500

    bootstrap out

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+/*import { Link } from 'react-router-dom';*/
+import classNames from 'classnames';
 import styles from './Nav.module.css';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const Nav = () => (
-  <nav className={styles.nav}>
-    <ul className={styles.list}>
-      <li><Link className={styles.link} to={"/"}>INICIO</Link></li>
-      <li><Link className={styles.link} to={"/about"}>SOBRE EL PROYECTO</Link></li>
-      <li><Link className={styles.link} to={"/help"}>¿CÓMO AYUDAR?</Link></li>
-      <li><Link className={styles.link} to={"/support"}>APOYAN</Link></li>
-      <li>REDES SOCIALES</li>
-    </ul>
-  </nav>
-);
+export default class TopNav extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default Nav;
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar className={styles.navbar} color="light" light expand="md">
+          <NavbarBrand className={'d-md-none d-xl-none'} href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse className={styles.collapse} isOpen={this.state.isOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <div className={'overlay'}></div>
+      </div>
+    );
+  }
+}
