14458378d767359632157aed336e2101b333cf0f
Carlos Andrés Rengifo Escobar <carlosandrex@gmail.com>
2018-08-12 16:21:51 -0500

    propagate device data to map component using react context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import AppContext from './context.js';
+
 import About from './Containers/About';
 import Home from './Containers/Home';
 import Help from './Containers/Help';
@@ -38,11 +40,18 @@ class App extends Component {
     return (
       <Router>
         <main>
-          <TopNav />
-          <Route exact path='/' render={() => <Home apiKey={this.props.apiKey} /> }></Route>
-          <Route path='/about' component={About}></Route>
-          <Route path='/help' component={Help}></Route>
-          <Route path='/support' component={Support}></Route>
+          <AppContext.Provider value={this.state.data}>
+            <TopNav />
+              <Route exact path='/' render={() => 
+                <AppContext.Consumer>
+                  {(data) => <Home apiKey={this.props.apiKey} data={data} />}
+                </AppContext.Consumer>
+              }>
+            </Route>
+            <Route path='/about' component={About}></Route>
+            <Route path='/help' component={Help}></Route>
+            <Route path='/support' component={Support}></Route>
+          </AppContext.Provider>
         </main>
       </Router>
     );
